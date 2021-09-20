e1363573cd379cd16b77903815d37ceabc644fc1
William <william@websnack.dk>
2021-09-20 10:44:45 +0200

    Update helpers.sh

--- helpers/helpers.sh
+++ helpers/helpers.sh
@@ -10,33 +10,21 @@ function install_or_update_composer_packages() {
   message "Cleaning composer..."
   composer clear-cache
 
-  # Install packages
-  composer require mageplaza/magento-2-danish-language-pack:dev-master
-  composer require mailchimp/mc-magento2
-  composer require bambora/module-payment-magento2
-  composer require amasty/mega-menu
-  composer require tric/module-autocity
-  composer require tric/module-economic
-  composer require tric/module-postnord
-  composer require tric/module-eanpayment
-
-  # Install Magento2 Gulp
-  composer require --dev bobmotor/magento-2-gulp
-
-  # For debug
-  composer require spatie/ray
+  # Install packages for base theme
+  # Amasty is required + the packages added in order to install packages 
+  composer require magento/data-migration-tool:^2.4.3 amasty/shopby amasty/mega-menu:^1.9 mageplaza/magento-2-danish-language-pack:dev-master avada/module-proofo mageplaza/module-core mageplaza/magento-2-product-slider amasty/module-single-step-checkout amasty/cart
 
   ## If true: Setup, Upgrade after install ##
   local setUpgrade=$1
 
   if [ "$setUpgrade" == true ]; then
       message "Setup: Deploy, reindex, clean & flush "
-      bin/magento setup:static-content:deploy -f
-      bin/magento setup:static-content:deploy da_DK -f
-      bin/magento indexer:reindex
-      bin/magento setup:upgrade
-      bin/magento cache:clean
-      bin/magento cache:flush
+      magerun2 setup:static-content:deploy -f
+      magerun2 setup:static-content:deploy da_DK -f
+      magerun2 indexer:reindex
+      magerun2 setup:upgrade
+      magerun2 cache:clean
+      magerun2 cache:flush
       message "Setup done"
   fi
 
